f996816f3c0c206577afaad6cf7d0ee447efb8b5
Bhim magar <shokokomichan098@gmail.com>
2025-05-01 21:20:55 +0545

    State commit

--- useState-hooks/src/TogglingUi/Toggle.jsx
+++ useState-hooks/src/TogglingUi/Toggle.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 
 function Toggle() {
-    const [isVisible,setIsVisible] = useState(false);
-    
-    const toggleVisibility = () => setIsVisible(!isVisible);
-
+    const[isVisible,setIsVisible] = useState(false);
+    const visible = () => setIsVisible(!isVisible);
   return (
     <div>
-        <button onClick={toggleVisibility} className='text-white text-2xl bg-blue-400 hover:bg-blue-500 border p-3 rounded-md'>{isVisible ? "hide" : "show"}</button>
-        {isVisible && <p className='text-white font-bold'>This is some Text Man. it can be toggled</p>};
+      <button onClick={visible} className='text-white text-2xl p-2 border-0 bg-green-400 hover:bg-green-500 rounded-sm'>{
+        isVisible ? "Show" : "Hide"}</button>
+        {isVisible && <p className='text-white font-bold'>This is a text and miso is sleeping</p>}
     </div>
   )
 }
