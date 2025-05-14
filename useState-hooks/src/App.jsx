db6fcc31e001a77f290dfa010f69a6b82844e5be
Bhim magar <shokokomichan098@gmail.com>
2025-05-14 19:50:02 +0545

    Sucess ko ghanti

--- useState-hooks/src/App.jsx
+++ useState-hooks/src/App.jsx
@@ -3,7 +3,9 @@ import ControlInput from './Advance/ControlInput'
 function App() {
   return (
     <div>
-      <ControlInput />
+      <ControlInput  />
+              className="text-xl bg-white block  outline-blue-400 outline-2 focus:outline-3 p-2 rounded-sm"
+
     </div>
   )
 }
