a943b0f626d56b2a3bb5e15bbbbb14d9adbd7c01
Bhim magar <shokokomichan098@gmail.com>
2025-05-17 19:08:39 +0545

    SimpleUiComplete

--- useState-hooks/src/Advance/ShoppingCard.jsx
+++ useState-hooks/src/Advance/ShoppingCard.jsx
@@ -3,9 +3,35 @@
 
     function ShoppingCard() {
       return (
-        <div>
-          <img src={imageOne} alt="" />
+        <>
+        <div className='flex bg-gray-500 p-3  rounded-sm w-70 shadow-2xl'>
+            <div>
+          <img src={imageOne} 
+          className='w-70'
+          alt="" />
+          <p className='font-semibold text-2xl text-white text-start my-3'>Oxygine Machine</p>
+          <p className='font-semibold text-2xl text-white text-start my-3 '>$41.99</p>
+          <p 
+          className='text-white text-sm text-start'
+          >This machine converts water and sunlight to the oxygen! Eco-Friendly too because they don't need any electricity. Aviables in the multiple shades of the green.</p>
+          <div className='flex justify-between'>
+            <button className='text-xl  bg-blue-500  w-10 text-white my-3 cursor-pointer hover:bg-blue-600 rounded-sm'>+</button>
+            <button
+            className='text-2xl text-white font-bold '
+            >0</button>
+            <button
+            className='text-xl  bg-red-500  w-10 text-white my-3 cursor-pointer hover:bg-red-600 rounded-sm'
+            >-</button>
+          <div>
+          </div>
+            <button
+            className='text-xl  bg-green-500  w-10 text-white my-3 cursor-pointer hover:bg-green-600 rounded-sm'
+            >Add</button>
+          </div>
+            </div>
+
         </div>
+        </>
       )
     }
     
