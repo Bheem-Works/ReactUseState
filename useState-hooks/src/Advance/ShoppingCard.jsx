776a9e5d275f9bfb1d55bdcce1e7d098436528fd
Bhim magar <shokokomichan098@gmail.com>
2025-05-20 01:43:54 +0545

    ShoppingCard

--- useState-hooks/src/Advance/ShoppingCard.jsx
+++ useState-hooks/src/Advance/ShoppingCard.jsx
@@ -1,7 +1,16 @@
-    import React from 'react'
+    import React, { useState } from 'react'
     import imageOne from '../assets/plant.png';
 
     function ShoppingCard() {
+
+      // Functionality 
+      const[count,setCount] = useState(0);
+      const [add,setAdd] = useState(0);
+      const increment = () => setCount(count => count + 1);
+      const decrement = () => setCount(count => count - 1);
+      const incrementAdd = () => {
+        setAdd(count);
+      }
       return (
         <>
         <div className='flex bg-gray-500 p-3  rounded-sm w-70 shadow-2xl'>
@@ -15,21 +24,31 @@
           className='text-white text-sm text-start'
           >This machine converts water and sunlight to the oxygen! Eco-Friendly too because they don't need any electricity. Aviables in the multiple shades of the green.</p>
           <div className='flex justify-between'>
-            <button className='text-xl  bg-blue-500  w-10 text-white my-3 cursor-pointer hover:bg-blue-600 rounded-sm'>+</button>
-            <button
+            <button 
+            onClick={increment}
+            className='text-xl  bg-blue-500  w-10 text-white my-3 cursor-pointer hover:bg-blue-600 rounded-sm'>+</button>
+            <button 
             className='text-2xl text-white font-bold '
-            >0</button>
-            <button
+            >{count}</button>
+            <button onClick={decrement}
             className='text-xl  bg-red-500  w-10 text-white my-3 cursor-pointer hover:bg-red-600 rounded-sm'
             >-</button>
           <div>
           </div>
-            <button
+            <button onClick={incrementAdd}
             className='text-xl  bg-green-500  w-10 text-white my-3 cursor-pointer hover:bg-green-600 rounded-sm'
             >Add</button>
           </div>
             </div>
 
+
+        </div>
+        <div>
+          {/* Shopping Card */}
+        <div>
+          {add}
+        <p>Shoppoing Card</p>
+        </div>
         </div>
         </>
       )
